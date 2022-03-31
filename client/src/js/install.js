fb483ae1bd0c0e871022ebbab64e27de46faf68d
Matt <86723332+mattmc86@users.noreply.github.com>
2022-03-31 09:48:32 +0100

    added event handlers for the install.js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,17 +1,28 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-   
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  window.deferredPrompt = event;
+  butInstall.style.visibility = "visible";
+  textHeader.textContent = "Click the button to install!";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
- 
+butInstall.addEventListener("click", async () => {
+  const Prompt_Event = window.deferredPrompt;
+  if (!Prompt_Event) {
+    return;
+  }
+  Prompt_Event.prompt();
+  window.deferredPrompt = null;
+  butInstall.setAttribute("disabled", true);
+  butInstall.textContent = "Installed";
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-  
+window.addEventListener("appinstalled", (event) => {
+  textHeader.textContent = "Successfully installed!";
+  console.log("👍", "appinstalled", event);
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,8 +32,8 @@ module.exports = () => {
         name: "Text-Editor",
         short_name: "Text-Editor",
         description: "Just a simple text-editor",
-        background_color: "#7eb4e2",
-        theme_color: "#7eb4e2",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
         icons: [
